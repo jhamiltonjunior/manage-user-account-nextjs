7ab747b9a5d43bb02e22f9bf26492e909eed3f63
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-02-02 20:14:58 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      spacing: {
-        "2/3": "66.666667%",
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
